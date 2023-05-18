097cb57cebd4d6222564bc6016e9fa5d00d09edc
Sara Khan Tareen <sara.tareen@gmail.com>
2023-05-18 12:21:34 +0500

    Added a form for the name, email and message. Made it functional using JS

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
     hamburger.classList.toggle("clicked");
   }
   
+
+function validateForm() {
+  let name = document.forms["myForm"]["enter_name"].value;
+  let email = document.forms["myForm"]["enter_email"].value;
+  let message = document.forms["myForm"]["enter_message"].value;
+  
+    alert(`
+    Name: ${name}
+    Email: ${email}
+    Message: ${message}`);
+}
